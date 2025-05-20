502590e99d635b2387a9e333df7858a667bb6781
Tusar-pal <tusarpal97@gmail.com>
2025-05-20 17:49:56 +0000

    string basic

--- besic/02_string.js
+++ besic/02_string.js
@@ -15,8 +15,29 @@ const str1 = new String ("tusar-pal");
 // console.log(str1.charAt(3))//index 3 number whoes string are store
 // console.log(str1.indexOf("u"));//index number find for "u"!
 
-const string = str1.substring(0,str1.length());
-console.log(string);
-let len = str1.length();
-const newstring =str1.slice( -len, 0)
-console.log(newstring);
+// const string = str1.substring(0,4);
+// console.log(string);
+// const newstring =str1.slice( -8, 0) reverse string 
+// const newstring =str1.slice( 1, 8) the input string
+// console.log(newstring);
+
+
+const newstrOne = "    tusar   ";
+// console.log(newstrOne);
+// console.log(newstrOne.trim());
+
+// const newSenstance = "Tusar is a software enigneer";
+// console.log(newSenstance.replace("Tusar" , "Akash"));
+
+
+// convert string to array
+
+// const nam = "tusar-pal-akash";
+// const word = nam.split(" ");
+// console.log(word[3]);
+
+const str = "The quick brown fox jumps over the lazy dog.";
+
+const words = str.split(" ");
+console.log(words);
+console.log(str.indexOf(1));
