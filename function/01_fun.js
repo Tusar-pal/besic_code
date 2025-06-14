25653d425728d254ec280f4f049a7813b03594a1
Tusar-pal <tusarpal97@gmail.com>
2025-06-14 17:16:24 +0000

    function

---
+++ function/01_fun.js
@@ -0,0 +1,10 @@
+function addTwonumbr(num1, num2){
+    if(typeof num1 == typeof num2){
+        const a=num1+num2;
+    console.log(a);
+    }else{
+        console.log("check the input");
+    }
+    
+}
+addTwonumbr(2,"1")
