674a9bc2d9446903e7c4ec6222b72e1b81ea834c
Tusar-pal <tusarpal97@gmail.com>
2025-06-15 15:15:52 +0000

    arrowfunction

---
+++ function/03_arrow.js
@@ -0,0 +1,30 @@
+const obj = {
+    name:"tusar",
+    email:"tusarpal@gamil.com",
+    welcome : function  (){
+        //console.log(`${this.name} welcome this website`);
+        //console.log(this);
+    }
+
+}
+obj.welcome()
+obj.name="akash";
+obj.welcome()
+//console.log(this);
+
+// function variable diclear
+const two = function (){
+    //console.log("enter the your name");
+}
+two();
+
+// arrow function -------------------->
+const one = (num1,num2)=>{
+    //console.log("enter the value ");
+    if (typeof num1 === typeof num2) {
+        return num1+num2;
+    }else{
+        console.log("check enter value");
+    }
+}
+console.log(one(1,3));
