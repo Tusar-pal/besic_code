5b23c3e418bfc0cd9d7f2e46b354c2ac5a25ecf7
Tusar-pal <tusarpal97@gmail.com>
2025-06-18 16:22:25 +0000

    call_stack

---
+++ function/call_stack.js
@@ -0,0 +1,15 @@
+function one (){
+    console.log("one");
+    tow();
+}
+function tow(){
+    console.log("tow");
+    // three();
+}
+function three(){
+    console.log("three")
+}
+
+one();
+tow();
+three();
