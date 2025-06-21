c9751f4ac3291b5d18b39fc2a0587235ad6dcfed
Tusar-pal <tusarpal97@gmail.com>
2025-06-21 18:39:38 +0000

    all loop

--- 03_iteration/for_in.js
+++ 03_iteration/for_in.js
@@ -9,10 +9,54 @@ let obj = {
         pin: 721301
     }
 };
-
+// for object
 for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        const element = obj[key];
-        console.log(key);
+       // console.log(`${key} for ${obj[key]}`);
     }
+
+
+// for array
+
+const arr1 =[1,3,25,43,12];
+for (const key in arr1) {
+    //console.log(`${key} for ${arr[key]}`);
+    }
+
+// for map ----------->
+
+let map = new Map();
+// it is not eligible for in loop because this map is a not iterated 
+map.set("In","India");
+map.set("USA","united state of America")
+map.set("AU","Australia");
+
+for (const key in map) {
+   // console.log(map[key])
 }
+
+//------------------>
+//for each
+const arr =["java","cpp","javascript","python"];
+arr.forEach( (item,index)=>{
+   // console.log(item,index)
+})
+
+// arr under object foreach use for axsis value---->
+
+const myCoding = [
+{
+        name : "java",
+        fileName : ".java"
+} ,
+{
+    name : "javascript",
+    fileName : ".js"
+},
+{
+    name:"python",
+    fileName : ".py"
+}
+]
+myCoding.forEach ( (item , index)=>{
+    console.log(item.fileName,item)
+})
