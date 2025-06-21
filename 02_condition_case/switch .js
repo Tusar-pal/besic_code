e88fcd458aae0ce1f9c655de3e25f3d7fd53eb69
Tusar-pal <tusarpal97@gmail.com>
2025-06-21 03:44:47 +0000

    switch

---
+++ 02_condition_case/switch .js
@@ -0,0 +1,28 @@
+const day =6;
+switch (day) {
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("mon");
+        break;
+    case 3:
+        console.log("tues");
+        break;
+    case 4:
+        console.log("Wednes");
+        break;
+    case 5:
+        console.log("thurs");
+        break;
+    case 6:
+        console.log("fri");
+        break;
+    case 7:
+        console.log("satur");
+        break;
+
+    default:
+        console.log("plase enter valid day")
+        break;
+}
