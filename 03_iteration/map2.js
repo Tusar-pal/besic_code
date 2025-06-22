8ca009fca63f9740394532654f73cde59b4a0fc3
Tusar-pal <tusarpal97@gmail.com>
2025-06-22 15:05:58 +0000

    map

--- 03_iteration/map2.js
+++ 03_iteration/map2.js
@@ -4,12 +4,12 @@ const NewNum = num.map((num)=>num+10)
 
 const num1 =[1,2,3,4,5,6,7,8,9,10];
 num1.forEach( (i)=>{
-    console.log(i+10)
+   // console.log(i+10)
 })
 
 // chaining method
 
-const newNums = myNumers
+const newNums = num
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
                 .filter( (num) => num >= 40)
