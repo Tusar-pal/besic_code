4de761354b8c0239455eb26ff4fa1028885cabf8
Tusar-pal <tusarpal97@gmail.com>
2025-06-23 03:34:51 +0000

    dom

--- DOM/01_DOM.js
+++ DOM/01_DOM.js
@@ -1,1 +1,18 @@
-document.getElementById('heading1').innerHTML = "<h1>Tusar pal</h1>";
+const ullist = document.querySelector('ul');
+ ullist.querySelectorAll('li').forEach((i)=>{
+    i.style.backgroundColor = "green"
+ })
+// ulist.forEach((i)=>{
+//     i.style.backgroundColor = 'yellow';
+// })
+
+const para = document.querySelector('#heading1');
+para.style.backgroundColor ="orange";
+para.innerText = "Tusar pal"
+
+// HTML collection convert in array(class attribute )
+// Array.from() ---------> use to convert HTMLcollection to array
+
+
+document.getElementById('heading1').style.color = 'blue'
+
