2035cc5a36d23ccca439d9aecee49fe2f8ae86ec
Tusar-pal <tusarpal97@gmail.com>
2025-06-24 10:09:04 +0000

    01_project

--- 01_project/01_project.js
+++ 01_project/01_project.js
@@ -3,36 +3,34 @@ const body = document.querySelector("body");
 button.forEach((i)=>{
   console.log(i);
   i.addEventListener("click",(j)=>{
-    console.log(j);
-    console.log(j.target)
     const color = j.target.id;
-    // switch(color){
-    //   case "grey":
-    //   body.style.backgroundColor = "grey";
-    //   break;
-    //   case "white":
-    //   body.style.backgroundColor = "white";
-    //   break;
-    //   case "blue":
-    //   body.style.backgroundColor = "blue";
-    //   break;
-    //   case "yellow":
-    //   body.style.backgroundColor = "yellow";
-    //   break;
-    // }
+    switch(color){
+      case "grey":
+      body.style.backgroundColor = "grey";
+      break;
+      case "white":
+      body.style.backgroundColor = "white";
+      break;
+      case "blue":
+      body.style.backgroundColor = "blue";
+      break;
+      case "yellow":
+      body.style.backgroundColor = "yellow";
+      break;
+    }
 
-    if(color === "grey"){
-      body.style.backgroundColor = color;
-    }
-    if(color === "yellow"){
-      body.style.backgroundColor = color;
-    }
-    if(color === "blue"){
-      body.style.backgroundColor = color;
-    }
-    if(color === "white"){
-      body.style.backgroundColor = color;
-    }
+    // if(color === "grey"){
+    //   body.style.backgroundColor = color;
+    // }
+    // if(color === "yellow"){
+    //   body.style.backgroundColor = color;
+    // }
+    // if(color === "blue"){
+    //   body.style.backgroundColor = color;
+    // }
+    // if(color === "white"){
+    //   body.style.backgroundColor = color;
+    // }
   })
 })
 
